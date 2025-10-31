3edd11ac146258da1ddda4b0f441b215ac900d7f
Isac Maximo <88113372+isacmaximo@users.noreply.github.com>
2025-10-31 11:39:04 -0300

    Removendo import não utilizado

--- lib/ui/cart/view_model/cart_view_model.dart
+++ lib/ui/cart/view_model/cart_view_model.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_cart_mvvm/domain/entities/cart_entity.dart';
 import 'package:flutter_shopping_cart_mvvm/domain/entities/cart_item_entity.dart';
